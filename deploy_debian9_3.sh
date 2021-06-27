9e8e0f0fda70345577a6ca5b6802175d703357ec
Igor Belousov <corlovito@gmail.com>
2021-06-27 12:45:16 +0300

    Commit 1.5

---
+++ deploy_debian9_3.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+wget https://panel.spaceproxy.net/static/install_3proxy_client2.sh -O /tmp/install_3proxy_client2.sh
+chmod +x /tmp/install_3proxy_client2.sh
+/bin/bash /tmp/install_3proxy_client2.sh $KEY_SERVER
+
