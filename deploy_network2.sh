25b6b9d39df6604333c11f0abbbb47ed9ee674e9
corlovito <radmyn@yandex.ru>
2021-10-22 11:24:31 +0300

    Edit file debian_network2

--- deploy_network2.sh
+++ deploy_network2.sh
@@ -14,12 +14,12 @@ interface=$(ext_interface)
 ip_address=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' > net
 
-net=$(sed 's/.$//' net)
+#net=$(sed 's/.$//' net)
 #net=$(echo $net | cut -c 1-12)
 
 echo "post-up /etc/network/ip-add-addresses" >> /etc/network/interfaces
 echo "#!/bin/bash" >> /etc/network/ip-add-addresses
 for ((i=4; i < 255; i++))
 do
-        echo ifconfig    $interface:$i $net$i      netmask 255.255.255.0 up >> /etc/network/ip-add-addresses
+        echo ifconfig    $interface:$i $1.$i      netmask 255.255.255.0 up >> /etc/network/ip-add-addresses
 done
