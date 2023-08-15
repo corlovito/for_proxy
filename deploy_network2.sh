65cd891b31289256142d3cb03e5d7e53ccda9cbe
Белоусов Игорь <radmyn@yandex.ru>
2023-08-15 18:47:14 +0300

    Fix deploy network for find name interface

--- deploy_network2.sh
+++ deploy_network2.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 apt-get install net-tools -y
 
-ext_interface () {
-    for interface in /sys/class/net/*
-    do
-        [[ "${interface##*/}" != 'lo' ]] && \
-            ping -c1 -W2 -I "${interface##*/}" 1.1.1.1 >/dev/null 2>&1 && \
-                printf '%s' "${interface##*/}" && return 0
-    done
-}
+interface=$(ip route show | awk '/default/ {print $5}')
 
-interface=$(ext_interface)
 ip_address=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' > net
 
@@ -23,3 +15,6 @@ for ((i=2; i < 255; i++))
 do
         echo ifconfig    $interface:$i $1.$i      netmask 255.255.255.0 up >> /etc/network/ip-add-addresses
 done
+
+chmod +x /etc/network/ip-add-addresses
+
